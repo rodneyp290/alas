d9f0b8d8cccf7a2380a7ab19d8b753bfeac049d6
Rodney Pilgrim <rodneyp290@gmail.com>
2016-12-14 12:13:23 +1100

    FIX: Set a default password of 'alas' for jupyter notebook

--- data/setup-pyspark-kernel.sh
+++ data/setup-pyspark-kernel.sh
@@ -1,6 +1,10 @@
 sudo jupyter kernelspec install /vagrant/data/jupyter/kernels/pyspark
 mkdir -p /home/vagrant/.ipython/profile_pyspark/startup/
 cp /vagrant/data/jupyter/kernels/pyspark/00-pyspark-setup.py /home/vagrant/.ipython/profile_pyspark/startup/
+mkdir /home/vagrant/.jupyter/
+# creates the password of 'alas' for the jupyter notebook server
+#  See http://jupyter-notebook.readthedocs.io/en/latest/public_server.html to create your own unique password
+echo "c.NotebookApp.password = 'sha1:241bb833225b:8bd5fd1393a441d327a8992fa1349e146e3c5af6'" >> /home/vagrant/.jupyter/jupyter_notebook_config.py
 
 echo spark.cores.max				4 	> /etc/apache-spark/spark-defaults.conf
 echo spark.driver.memory			6g 	>> /etc/apache-spark/spark-defaults.conf
