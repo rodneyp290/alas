d574bb65ce6970e04ffad4b1a68c1b074463e04a
Rodney Pilgrim <rodneyp290@gmail.com>
2016-12-14 17:06:42 +1100

    FIX: Start up Jupyter Notebook file permissions, plus added new quick run-jupyter script

--- data/setup-pyspark-kernel.sh
+++ data/setup-pyspark-kernel.sh
@@ -1,10 +1,19 @@
 sudo jupyter kernelspec install /vagrant/data/jupyter/kernels/pyspark
+
 mkdir -p /home/vagrant/.ipython/profile_pyspark/startup/
 cp /vagrant/data/jupyter/kernels/pyspark/00-pyspark-setup.py /home/vagrant/.ipython/profile_pyspark/startup/
+
 mkdir /home/vagrant/.jupyter/
 # creates the password of 'alas' for the jupyter notebook server
 #  See http://jupyter-notebook.readthedocs.io/en/latest/public_server.html to create your own unique password
 echo "c.NotebookApp.password = 'sha1:241bb833225b:8bd5fd1393a441d327a8992fa1349e146e3c5af6'" >> /home/vagrant/.jupyter/jupyter_notebook_config.py
+
+echo "jupyter notebook --no-browser --ip=0.0.0.0" >> /home/vagrant/run-jupyter
+
+chown -R vagrant:vagrant /home/vagrant/.ipython/
+chown -R vagrant:vagrant /home/vagrant/.jupyter/
+chown -R vagrant:vagrant /home/vagrant/run-jupyter
+chmod +x /home/vagrant/run-jupyter
 
 echo spark.cores.max				4 	> /etc/apache-spark/spark-defaults.conf
 echo spark.driver.memory			6g 	>> /etc/apache-spark/spark-defaults.conf
